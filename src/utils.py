35698216cce23a0f1be3040a582a6e3b753fa399
afsharino <mohammadafshari.fum.ac.ir@gmail.com>
2024-06-02 16:53:06 +0330

    make some changes to read_data function in utils module and read data in main (fix bug).

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,34 @@
 # Import libraries
 import os
 import json
+import glob
 
-# A function for reading datasets
-def read_data(BASE_DIR):
+#_____________ read_data _____________
+def read_data(base_dir:str) -> dict:
+    """A function for reading datasets
+
+    Args:
+        base_dir (str): directory that contains sub-directories of datasets
+
+    Returns:
+        dict: a dictionary of data  key(dir_name) : value(key(indiactor_name) : value(indicator_Values))
+    """
     data = dict()
-    for dir in os.listdir(BASE_DIR):
-        if 'csv' in dir or '.ipynb_checkpoints' in dir:
+    
+    # Check if the directory is empty or not
+    if os.listdir(base_dir) == []:
+        print(f"The directory is empty!\n")
+        return
+
+    for dir_name in os.listdir(base_dir):
+        # do not read csv file which contains fear and greed indicator
+        if 'csv' in dir_name:
             continue
-        print(f"\n\n\nEnter ____ {dir} ____ Directory")
-        data[dir] = {}
-        dir_path = os.path.join(BASE_DIR, dir)
+
+        # Enter directories and read indicators one by one
+        print(f"\n\n\nEnter ____ {dir_name} ____ Directory")
+        data[dir_name] = {}
+        dir_path = os.path.join(base_dir, dir_name)
         for file_path in glob.glob(os.path.join(dir_path, '*.json')):
             name = file_path.split('\\')[-1].split(".")[0].strip()
             print(f"Start Reading {name}...")
@@ -18,6 +36,7 @@ def read_data(BASE_DIR):
             with open(file_path, 'r') as file:
                 # Load json file
                 json_data = json.load(file)['data']
-                data[dir][name] = json_data
+                data[dir_name][name] = json_data
                 print(f"{name} loaded succesfully :)\n")
+
     return data
