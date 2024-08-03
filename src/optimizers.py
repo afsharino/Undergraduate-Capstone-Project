5db9e4ec4104485e69b7a4e2720ecbce971e45b1
afsharino <mohammadafshari.fum.ac.ir@gmail.com>
2024-08-03 18:28:58 +0330

    add new module models and move nn to models module

--- src/optimizers.py
+++ src/optimizers.py
@@ -2,17 +2,15 @@
 import numpy as np
 import pygad
 import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras import layers
-#from tensorflow.python.keras import layers
 
 #__________________________  Use Linear Combination as a Model __________________________
 # Define the fitness function
 def linear_fitness_function(ga_instance, solution, solution_idx):
     new_indicator = np.dot(linear_data, solution)
+
+    # Normalize new_indicator to range [0, 100]
     min_value = np.min(new_indicator)
     max_value = np.max(new_indicator)
-    # Normalize new_indicator to range [0, 100]
     new_indicator = ((new_indicator - min_value) / (max_value - min_value)) * 100
     
     INITIAL_BALANCE = 10000
@@ -25,13 +23,11 @@ def linear_fitness_function(ga_instance, solution, solution_idx):
         indicator_value = new_indicator[i]
         price = linear_prices[i]
         
-        total_balance =  cash_balance + (bitcoin_amount*price)
-        
+        total_balance =  cash_balance + (bitcoin_amount * price)
         bitcoin_amount = ((indicator_value/100) * total_balance) / price
-        
-        cash_balance = total_balance - (bitcoin_amount*price)
-        
+        cash_balance = total_balance - (bitcoin_amount * price)
         profit = total_balance - INITIAL_BALANCE
+
     return profit
 
 def linear_genetic_algorithm(data_param, prices_param, num_individuals, num_genes, num_generations, mutation_rate, initial_population):
@@ -77,67 +73,33 @@ def linear_genetic_algorithm(data_param, prices_param, num_individuals, num_gene
     return fitness_values, best_fitness, best_coefficients
 
 
-#__________________________  Use MLP as a Model __________________________
-class NeuralNetwork:
-    def __init__(self, input_layer_size, hidden_layer_size, output_layer_size):
-        # Initialize weights and biases
-        self.weights_input_hidden = np.random.randn(input_layer_size, hidden_layer_size)
-        self.bias_hidden = np.random.randn(hidden_layer_size)
-        self.weights_hidden_output = np.random.randn(hidden_layer_size, output_layer_size)
-        self.bias_output = np.random.randn(output_layer_size)
-    
-    def relu(self, x):
-        return np.maximum(0, x)
-    
-    def forward(self, X):
-        # Forward pass
-        self.hidden_layer_input = np.dot(X, self.weights_input_hidden) + self.bias_hidden
-        self.hidden_layer_output = self.relu(self.hidden_layer_input)
-        self.output_layer_input = np.dot(self.hidden_layer_output, self.weights_hidden_output) + self.bias_output
-        self.output_layer_output = self.relu(self.output_layer_input)
-        return self.output_layer_output
-
-    def set_weights(self, weights):
-        input_hidden_end = self.weights_input_hidden.size
-        self.weights_input_hidden = weights[:input_hidden_end].reshape(self.weights_input_hidden.shape)
-        
-        bias_hidden_end = input_hidden_end + self.bias_hidden.size
-        self.bias_hidden = weights[input_hidden_end:bias_hidden_end]
-        
-        hidden_output_end = bias_hidden_end + self.weights_hidden_output.size
-        self.weights_hidden_output = weights[bias_hidden_end:hidden_output_end].reshape(self.weights_hidden_output.shape)
-        
-        self.bias_output = weights[hidden_output_end:]
-    
-    def get_weights(self):
-        return np.concatenate([
-            self.weights_input_hidden.flatten(),
-            self.bias_hidden,
-            self.weights_hidden_output.flatten(),
-            self.bias_output
-        ])
-    
+#__________________________  Use MLP as a Model __________________________   
 # Define the fitness function
 def mlp_fitness_function(ga_instance, solution, solution_idx):
     nn.set_weights(solution)
     predictions = nn.forward(mlp_data)
     
     # Normalize predictions
-    predictions = ((predictions - predictions.min()) / (predictions.max() - predictions.min())) * 100
-    #predictions = predictions.reshape(-1)
-    
+    min_value = np.min(predictions)
+    max_value = np.max(predictions)
+    # Normalize new_indicator to range [0, 100]
+    predictions = ((predictions - min_value) / (max_value - min_value)) * 100
+
     INITIAL_BALANCE = 10000
     cash_balance = INITIAL_BALANCE
-    bitcoin_amount = np.zeros(len(mlp_prices))
-    total_balance = np.zeros(len(mlp_prices))
+    bitcoin_amount = 0
+    total_balance = cash_balance
+    profit = 0
+    
+    for i in range(len(linear_prices)):
+        indicator_value = predictions[i]
+        price = linear_prices[i]
+        
+        total_balance =  cash_balance + (bitcoin_amount * price)
+        bitcoin_amount = ((indicator_value/100) * total_balance) / price
+        cash_balance = total_balance - (bitcoin_amount * price)
+        profit = total_balance - INITIAL_BALANCE
 
-    total_balance[0] = INITIAL_BALANCE
-    for i in range(1, len(mlp_prices)):
-        total_balance[i] = cash_balance + bitcoin_amount[i-1] * mlp_prices[i]
-        bitcoin_amount[i] = (predictions[i] / 100) * total_balance[i] / mlp_prices[i]
-        cash_balance = total_balance[i] - bitcoin_amount[i] * mlp_prices[i]
-
-    profit = total_balance[-1] - INITIAL_BALANCE
     return profit
 
 def mlp_genetic_algorithm(data_param, prices_param, num_individuals, num_genes, num_generations, mutation_rate, initial_population, model):
